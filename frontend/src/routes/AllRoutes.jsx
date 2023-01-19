2eeba6e58015fe4e07416cb03a9ffb04ddb969ac
gauravsudhanshu <gauravsud3@gmail.com>
2023-01-19 11:16:55 +0530

    sale

--- frontend/src/routes/AllRoutes.jsx
+++ frontend/src/routes/AllRoutes.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Homepage from "../pages/Homepage";
+import Signup from "../pages/Signup";
 
 export default function AllRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Homepage></Homepage>}></Route>
       <Route path="*" element={<h1>Page Not Found</h1>}></Route>
+      <Route path="/signup" element={<Signup></Signup>}></Route>
     </Routes>
   );
 }

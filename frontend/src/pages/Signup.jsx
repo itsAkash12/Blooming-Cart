d69a5c9fed806e07f63d6ba78e203029094f9cca
gauravsudhanshu <gauravsud3@gmail.com>
2023-01-19 18:05:36 +0530

    signup

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-
-const Signup = () => {
-  return (
-    <div>
-      <h1>import axios from"axios"
+import axios from"axios"
 import {FcGoogle, }from "react-icons/fc"
 import {FaMicrosoft }from "react-icons/fa"
-import {FaApple }from "react-icons/fa"
 import {AiOutlineEye }from "react-icons/ai"
 import {AiOutlineEyeInvisible }from "react-icons/ai"
+import signup_cover from "../assets/images/signup_cover.jpg"
+import signup_backcover from "../assets/images/signup_backcover.jpg"
+import navbar_img from "../assets/images/navbar_img.jpg"
 
 import {
     Button,
@@ -29,13 +27,13 @@ import {
   } from '@chakra-ui/react'
 import { useState } from "react"
 
-import logo from "../GsAssets/navbarLogo.jpg";
-import {  useNavigate } from "react-router-dom"
+import logo from "../assets/images/blooming_cart.jpg";
+import {  useNavigate } from "react-router-dom";
 
-  // 
 
-const Signup=()=>{
- const [open,setOpen]=useState(false)
+
+const Signup = () => {
+  const [open,setOpen]=useState(false)
  const [error, changeError] = useState("")
   const navigate = useNavigate()
  const toggle=()=>{
@@ -43,6 +41,8 @@ const Signup=()=>{
  }
   const [user,setUser]=useState({
     username:"",
+    firstname:"",
+    lastname:"",
     email:"",
     password:""
 })
@@ -54,8 +54,10 @@ setUser({...user,[name]:value})
 
 const handleSubmit=(e)=>{
   e.preventDefault()
-  axios.post("https://five-metric.onrender.com/signup",{
+  axios.post("https://blooming_cartcom/signup",{
     username:user.username,
+    firstname:user.firstname,
+    lastname:user.lastname,
     email:user.email,
     password:user.password
   })
@@ -72,10 +74,11 @@ const handleSubmit=(e)=>{
      console.log(err)
   })
 }
+  return (
 
-    return(
-      <Stack bg={"#f6f7f8"}>
-        <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }} border={"1px solid lightgrey"} w={"85%"} margin="auto" borderRadius={"5px"} marginTop={"5px"} >
+    <>
+      <Stack bgImage={signup_backcover}>
+        <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }} border={"1px solid lightgrey"} w={"85%"} margin="auto"  borderRadius={"5px"} marginTop={"5px"} >
         <Flex  flex={1} align={'center'} justify={'center'}  bg={"white"} borderRadius={"5px"}>
           <Stack spacing={4} w={'65%'} maxW={'md'} pt="50px"> 
            <Link onClick={()=>navigate("/")}>
@@ -83,12 +86,17 @@ const handleSubmit=(e)=>{
             <Heading fontSize={'1.625rem'} fontWeight={"16px"} color={"black"}>Create Your Account</Heading>
           <form onSubmit={handleSubmit}>
             <FormControl >
-              <FormLabel fontSize={"14px"} color={"#777e85"}> User Name</FormLabel>
-              <Input type="text" height={"34px"} placeholder={"John Smith"} value={user.username} name="username" onChange={handleChange}/>
+              <FormLabel fontSize={"14px"} color={"#777e85"}> First Name</FormLabel>
+              <Input type="text" height={"34px"} placeholder={"Gaurav"} value={user.firstname} name="firstname" onChange={handleChange}/>
             </FormControl>
             <FormControl >
+              <FormLabel fontSize={"14px"} color={"#777e85"}> Last Name</FormLabel>
+              <Input type="text" height={"34px"} placeholder={"Sudhanshu"} value={user.lastname} name="lastname" onChange={handleChange}/>
+            </FormControl>
+            
+            <FormControl >
               <FormLabel fontSize={"14px"} color={"#777e85"}>Email </FormLabel>
-              <Input type="email"  height={"34px"}  placeholder="johnsmith@gmail.com" value={user.email} name="email" onChange={handleChange}/>
+              <Input type="email"  height={"34px"}  placeholder="gs@gmail.com" value={user.email} name="email" onChange={handleChange}/>
             </FormControl>
             <FormControl >
               <FormLabel fontSize={"14px"} color={"#777e85"}>Password</FormLabel>
@@ -96,32 +104,32 @@ const handleSubmit=(e)=>{
               <Input type={(open===false)?"password":"text"} height={"34px"}  placeholder="Enter your password" value={user.password} name="password" onChange={handleChange}/>
             <InputRightElement><Box>{(open===false)? <AiOutlineEyeInvisible onClick={toggle} />:<AiOutlineEye onClick={toggle}/>}</Box></InputRightElement>  
               </InputGroup>
-             
             </FormControl>
 
               <Stack
                 direction={{ base: 'column', sm: 'row' }}
                 align={'start'}
                 justify={'space-between'}>
-                <Checkbox fontSize={"14px"} color={"#777e85"} marginTop={"5px"}>I accept <span style={{color:"#3070F0",fontSize:"14px",fontWeight:"600"}}>Terms of Service</span></Checkbox>
+                <Checkbox fontSize={"14px"} color={"#777e85"} marginTop={"5px"}> <span style={{color:"#3070F0",fontSize:"14px",fontWeight:"600"}}>Email me about special offers, new products and promotions.</span></Checkbox>
               
               </Stack>
               <Text color="red">{error}</Text>
               <Button colorScheme={'blue'} variant={'solid'} height={"34px"} fontSize={"14px"} w={"100%"} marginTop="20px" type="submit" >
-                Sign Up
+                Create your Account
               </Button>
         </form>
+        
               <Divider orientation={'horizontal'}/>
               <Stack>
-              <Button colorScheme={"none"} border={"1px solid lightgrey"} _hover={{bg: '#e2e6eb',}} leftIcon={<FcGoogle fontSize={"20px"} /> } textColor={"#777E85"} variant={'solid'} height={"34px"} fontSize={"14px"}>
+              <Button colorScheme={"none"} border={"1px solid lightgrey"} _hover={{bg: '#e2e6eb',}} leftIcon={<FcGoogle fontSize={"20px"} /> } textColor={"#777E85"} variant={'solid'} height={"34px"} fontSize={"14px"} >
               Sign Up with Google
               </Button>
               <Button colorScheme={"none"} border={"1px solid lightgrey" } _hover={{bg: '#e2e6eb',}} leftIcon={<FaMicrosoft  fontSize={"20px"}/>} textColor={"#777E85"} variant={'solid'} height={"34px"} fontSize={"14px"}>
               Sign Up with Microsoft
               </Button>
-              <Button colorScheme={"none"} border={"1px solid lightgrey"} _hover={{bg: '#e2e6eb',}} leftIcon={<FaApple color={"black"} fontSize={"20px"}/>} textColor={"#777E85"} variant={'solid'} height={"34px"} fontSize={"14px"}>
+              {/* <Button colorScheme={"none"} border={"1px solid lightgrey"} _hover={{bg: '#e2e6eb',}} leftIcon={<FaApple color={"black"} fontSize={"20px"}/>} textColor={"#777E85"} variant={'solid'} height={"34px"} fontSize={"14px"}>
               Sign Up with Apple
-              </Button>
+              </Button> */}
               </Stack>
               <Divider orientation={'horizontal'}  color={"blue"}/>
            <Heading fontSize={'14px'} fontWeight={"10px"} paddingBottom="20px">Already have an account?<Link onClick={()=>navigate("/login")} color="blue"> Login</Link></Heading>
@@ -131,12 +139,15 @@ const handleSubmit=(e)=>{
         </Flex>
         <Flex flex={1}>
           <Image
-          backgroundColor={"#f2f5fc"}
-            alt={'Login Image'}
+          backgroundColor={"#4b5f54"}
+            alt={'Signup Image'}
             objectFit={'cover'}
-            borderRadius={"5px"}
+            borderRadius={"15px"}
+            width={"90%"}
+            height={"96%"}
+            margin={"20px"}
             src={
-                'https://id.tmetric.com/images/side_image_register.svg'
+              signup_cover
             }
           />
            
@@ -145,46 +156,8 @@ const handleSubmit=(e)=>{
       
       </Stack>
       </Stack>
-    )
-}
-export default Signup
-
-
-
-
-
-
-// <div className="signup_main_box">
-//     <div className="signup_main_box1">
-//         <h2>Create Your Account</h2>
-//         <form>
-//         <label>Name</label>
-//         <input  type="text"placeholder="name"/>
-//         <br></br>
-//         <label>Email</label>
-//         <input type="text" placeholder="email"/>
-//         <br></br>
-//         <label>Password</label>
-//         <input  type="password"placeholder="password"/>
-//         <br></br>
-//         <input type="submit" value="Sign Up"/>
-//         <input type="checkbox"/>
-//         </form>
-//         <p>or</p>
-//         <button>Signu Up with Google</button>
-//         <br></br>
-//         <button>Sign Up with Microsoft</button>
-//         <br></br>
-//         <button>Signu Up with Apple</button>
-
-//         <p>Already have an account? </p>
-//         <Link to="/login">Login</Link>
-        
-     
-//     </div>
-//     <div className="signup_main_box2"></div>
-// </div></h1>
-    </div>
+     <Stack><Image src={navbar_img}/></Stack>
+    </>
   )
 }
 

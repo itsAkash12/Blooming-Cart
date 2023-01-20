3cf6ec76a4e19a70e550ccb3669fe92fa5eb1b7d
kirti2818 <104035990+kirti2818@users.noreply.github.com>
2023-01-20 15:23:13 +0530

    day-5

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -12,16 +12,15 @@
 //     "sizes":""
 const mongoose = require("mongoose")
 const productSchema = new mongoose.Schema({
-   image : {type : Array , required : true},
-   product_name : {type : String , required : true},
+   image : {type : String , required : true},
+   productname : {type : String , required : true},
    price : {type : String,required : true},
    strike_price : {type : Number},
-   rating : {type : Number,required : true},
-   colors : {type : String , required : true},
    delivery_time : {type : Number , default : 3},
-   sizes : {type : String ,required : true},
+   size : {type : String ,required : true},
    category:{type:String , required:true},
-   description:{type : String , required : true}
+   description:{type : String , required : true},
+   multi_image:{type:Array}
 
 })
 const Products = mongoose.model("product" , productSchema)

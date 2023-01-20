7947314941f5405aaee74b963d189f0f5df3f656
Rahulzhp <akumari42872@gmail.com>
2023-01-20 13:45:20 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,11 @@ const port = process.env.port
 const cors = require("cors")
 const bcrypt = require('bcrypt');
 app.use(express.json())
+app.use(cors())
 app.use("/users",userRoutes)
 app.use("/products",productRoutes)
 app.use("/carts",cartRoutes)
-app.use(cors())
+
 
 
 app.listen(port , async()=>{

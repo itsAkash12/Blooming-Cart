01416eb195b169624aedfe774dadc309af8f6c64
Rahulzhp <akumari42872@gmail.com>
2023-01-20 17:17:26 +0530

    productpage

--- frontend/src/pages/Indvidual.jsx
+++ frontend/src/pages/Indvidual.jsx
@@ -15,9 +15,11 @@ import { useState,useEffect, useRef } from 'react';
 import Productdata from "./Productdata"
 
 import axios from "axios";
+import { useNavigate } from 'react-router';
 
 
 const Indvidual = () => {
+  let navigate = useNavigate();
   const [data,setData]=useState([]);
   const [modal, setModal] = useState(false);
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -35,6 +37,7 @@ const Indvidual = () => {
   }
   const handleshow=(id)=>{
     console.log(id)
+    navigate(`/${id}`)
     
   }
 
@@ -53,23 +56,23 @@ const Indvidual = () => {
                     {
                        data && data.map((el)=>(
                             <Box  key={el._id} w="100%" textAlign="center">
-                                <Box onClick={() => { onOpen(); hanldeEdit() }} cursor={"pointer"} w="75%" m="auto">
-                                <Image onClick={()=>handleshow(el._id)} w="100%" src={el.image}></Image>
+                                <Box onClick={()=>handleshow(el._id)} cursor={"pointer"} w="75%" m="auto">
+                                <Image  w="100%" src={el.image}></Image>
                                 </Box> 
                                 <Box  fontSize={["15px","16px","16px"]}  fontWeight="semibold" h={["41px","46px","48px"]} overflow="hidden">
-                                <Text p="0px 7px" >{el.description}</Text>
+                                <Text p="3px 7px" >{el.productname}</Text>
                                 </Box>
                                 <Box w="39%" display={"flex"} gap="5px" margin="auto" alignItems="center" justifyContent="center">
                                   <Text fontSize="17px">{el.ratings}</Text>
                                     <Image w="100%" src="https://static.vecteezy.com/system/resources/previews/004/256/658/original/five-star-customer-product-ratings-review-flat-icons-for-apps-and-websites-illustration-of-five-golden-yellow-stars-in-a-row-isolated-in-a-white-background-concepts-for-ratings-customers-review-free-vector.jpg"></Image>
                                     </Box>
                                     <Box display="flex" alignItems="center" justifyContent="center">
-                                    <Text color="blue"  fontSize="17px">  ₹ {35} /-</Text>
-                                    <Text as="s" m="3px 4px" fontSize="14px"> ₹ {21*3}/-</Text>
+                                    <Text color="blue"  fontSize="17px">  $ {el.price} /-</Text>
+                                    <Text as="s" m="3px 4px" fontSize="14px"> $ {Math.floor(el.strike_price*5)}/-</Text>
                                     </Box>
                                 <Box display="flex" m="auto" alignItems="center" justifyContent="center">
                                 
-                                <Button onClick={()=>handleCart(el.id)} backgroundColor={process.env.REACT_APP_BG_COLOR} mr="5px"  size='sm'>
+                                <Button onClick={()=>handleCart(el._id)} backgroundColor={process.env.REACT_APP_BG_COLOR} mr="5px"  size='sm'>
                                     Add to Cart
                                 </Button>
                                 <Button  color={process.env.REACT_APP_BG_COLOR} size='sm'>
@@ -79,23 +82,7 @@ const Indvidual = () => {
                             </Box>
                         ))
                     } 
-                   <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Create your account</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
-            
-          </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme='blue' mr={3}>
-              Save
-            </Button>
-            <Button onClick={onClose}>Cancel</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+                   
                 </SimpleGrid>
             </Wrap>
     </div>
--- frontend/src/pages/Productdata.jsx
+++ frontend/src/pages/Productdata.jsx
@@ -2,26 +2,33 @@ import React from 'react'
 import { useState,useEffect, useRef } from 'react';
 import Indvidual from './Indvidual';
 import axios from "axios";
-import { Box, HStack } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
+import { useParams } from 'react-router-dom';
 
-const Productdata = (id) => {
-  console.log(id)
+const Productdata = () => {
+  const {id}=useParams();
+  console.log("param",id)
+
+ 
+  
     const [data,setData]=useState([]);
 
     useEffect(()=>{
-        axios.get("http://localhost:8080/products?category=birthday")
+        axios.get(`http://localhost:8080/products/${id}`,{
+          headers: {
+            'Authorization': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzY2E3NzMzYjIyMjlmMWM1YTAzZTRkNiIsInJvbGUiOiJFeHBsb3JlciIsImVtYWlsIjoic3JAZ21haWwuY29tIiwibmFtZSI6InJhaHVsIiwiaWF0IjoxNjc0MjEzMjA2fQ.BfAMpuadLZfdULOXvlXTDIyQPUL2dlg3DZmFB6-VypA"
+        }
+        })
         .then((res)=>setData(res.data))
         .catch((er)=>console.log(er))
       },[])
       console.log(data)
   return (
     <Box>
-        {
-          data && data.map((el)=>( 
-            <Indvidual key={el._id} id={el._id} img={el.image} desc={el.description} 
-            size={el.size} name={el.product_name} del={el.delivery_time}></Indvidual> 
-          ))
-        }
+        <Box w="90%" margin="auto" display={"flex"}>
+          <Box w="50%" h="200px" border="1px solid red"></Box>
+          <Box w="50%"  h="200px" border="1px solid yellow"></Box>
+        </Box>
     </Box>
   )
 }

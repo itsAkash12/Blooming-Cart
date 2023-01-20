883e730064f5e878701237d262d7f0fbf39ce4af
gauravsudhanshu <gauravsud3@gmail.com>
2023-01-20 13:27:24 +0530

    login_

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const productRoutes = require("./routes/product.route")
 const port = process.env.port
 const bcrypt = require('bcrypt');
 app.use(express.json())
+
 app.use("/users",userRoutes)
 app.use("/products",productRoutes)
 

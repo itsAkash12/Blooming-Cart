20cd710c616091b4d51abd1fd27bf05c57d227a6
Akash Kumar Singh <akashviratsingh018@gmail.com>
2023-01-21 18:20:46 +0530

    added: graphs

--- frontend/src/components/admin/LineChart.jsx
+++ frontend/src/components/admin/LineChart.jsx
@@ -3,8 +3,8 @@ import {Box} from "@chakra-ui/react"
 import { Line } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 
-const LineChart = ({chartData}) => {
+const PieChart = ({chartData}) => {
   return (<Line data={chartData} />)
 }
 
-export default LineChart
+export default PieChart
